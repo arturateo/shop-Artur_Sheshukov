10116b22ac6925a252b7c0d5a1e00618cd8de075
Artur Sheshukov <arti.shishka11@gmail.com>
2023-06-25 02:55:28 +0600

    Добавил вьюхи и шаблоны страниц для добавления продукта и категории

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from shop.models import Product
+from shop.models import Product, Category
 
 
 def products_view(request):
@@ -14,8 +14,26 @@ def product_view(request, *args, **kwargs):
 
 
 def category_add_view(request):
-    return render(request, "category_add.html")
+    if request.method == "GET":
+        return render(request, "category_add.html")
+    else:
+        Category.objects.create(
+            title=request.POST.get('title'),
+            description=request.POST.get('description')
+        )
+        return redirect("home")
 
 
 def product_add_view(request):
-    return render(request, "product_add.html")
+    if request.method == "GET":
+        categories = Category.objects.all()
+        return render(request, "product_add.html", {'categories': categories})
+    else:
+        Product.objects.create(
+            title=request.POST.get('title'),
+            description=request.POST.get('description'),
+            price_coast=request.POST.get('price'),
+            pic_img=request.POST.get('pic_img'),
+            select_id=request.POST.get('category'),
+        )
+        return redirect("home")
